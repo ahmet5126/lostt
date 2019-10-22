bab6558967edac6d46c46728c03fe55f573f4ca2
Glitch (discord-bot-altyapi) <none>
2019-10-22 10:30:38 +0000

    🌈🏬 Checkpoint ./ayarlar.json:1302778/113 ./package.json:1302778/195801 ./events/ready.js:1302778/117

--- events/ready.js
+++ events/ready.js
@@ -7,16 +7,16 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "g!yardım - Otorol Güncellendi🔥",
-        "g!yardım - Küfür Engel✨",
-        "g!yardım - Reklam Engel⭐️"  
+        "ash bot altyapı",
+        "",
+        ""  
     ];
 
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://www.twitch.tv/emirhansaracyt" );
+        client.user.setActivity(oyun[random], "https://www.twitch.tv/fatihcann" );
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
